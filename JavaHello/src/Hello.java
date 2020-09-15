56646356b59116ac80edae41b9777119f0dcadf5
Ashank16 <ashankjuyal@gmail.com>
2020-09-15 12:43:01 +0530

    JAVA Programs 1

---
+++ JavaHello/src/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Ashank Juyal\n45-IT-18");
+    }
+    
+}
---
+++ JavaSquareroot/src/Squareroot.java
@@ -0,0 +1,12 @@
+import java.lang.Math;
+
+public class Squareroot {
+
+    public static void main(String[] args) {
+        double x = 25;
+        double y = Math.sqrt(x);
+        System.out.println("Square root is : " + y);
+        System.out.println("Ashank Juyal\n45-IT-18");
+    }
+    
+}
